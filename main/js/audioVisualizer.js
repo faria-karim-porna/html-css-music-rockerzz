24b9915eb268f09f5294fac84fbad23d55bad911
Faria-karim-porna <faria-karim-porna@users.noreply.github.com>
2022-02-06 15:18:25 +0000

    Audio Visualizer Updated

--- main/js/audioVisualizer.js
+++ main/js/audioVisualizer.js
@@ -1,45 +1,70 @@
-function start() {
-  var context = new (window.AudioContext || window.webkitAudioContext)();
-  var analyser = context.createAnalyser();
+function playSong() {
+  // The number of bars that should be displayed
+  const NBR_OF_BARS = 50;
 
-  var ctx = document.querySelector("canvas").getContext("2d");
+  // Get the audio element tag
+  const audio = document.querySelector("audio");
+  // audio.load();
 
-  var numPoints = analyser.frequencyBinCount;
-  var heightArray = new Uint8Array(numPoints);
+  // Create an audio context
+  const ctx = new AudioContext();
 
-  function render() {
-    analyser.getByteFrequencyData(heightArray);
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-    ctx.clearRect(0, 0, width, height);
-    for (var x = 0; x < width; x += 10) {
-      var ndx = ((x * numPoints) / width) | 0;
-      var vol = heightArray[ndx];
-      var y = (vol * height) / 255;
-      ctx.fillRect(x, y, 10, 10);
+  // Create an audio source
+  const audioSource = ctx.createMediaElementSource(audio);
+
+  // Create an audio analyzer
+  const analayzer = ctx.createAnalyser();
+
+  // Connect the source, to the analyzer, and then back the the context's destination
+  audioSource.connect(analayzer);
+  audioSource.connect(ctx.destination);
+
+  // Print the analyze frequencies
+  const frequencyData = new Uint8Array(analayzer.frequencyBinCount);
+  analayzer.getByteFrequencyData(frequencyData);
+  console.log("frequencyData", frequencyData);
+
+  // Get the visualizer container
+  const visualizerContainer = document.querySelector(".visualizer-container");
+
+  // Create a set of pre-defined bars
+  for (let i = 0; i < NBR_OF_BARS; i++) {
+    const bar = document.createElement("DIV");
+    bar.setAttribute("id", "bar" + i);
+    bar.setAttribute("class", "visualizer-container__bar");
+    visualizerContainer.appendChild(bar);
+  }
+
+  // This function has the task to adjust the bar heights according to the frequency data
+  function renderFrame() {
+    // Update our frequency data array with the latest frequency data
+    analayzer.getByteFrequencyData(frequencyData);
+
+    for (let i = 0; i < NBR_OF_BARS; i++) {
+      // Since the frequency data array is 1024 in length, we don't want to fetch
+      // the first NBR_OF_BARS of values, but try and grab frequencies over the whole spectrum
+      const index = (i + 10) * 2;
+      // fd is a frequency value between 0 and 255
+      const fd = frequencyData[index];
+
+      // Fetch the bar DIV element
+      const bar = document.querySelector("#bar" + i);
+      if (!bar) {
+        continue;
+      }
+
+      // If fd is undefined, default to 0, then make sure fd is at least 4
+      // This will make make a quiet frequency at least 4px high for visual effects
+      const barHeight = Math.max(4, fd || 0);
+      bar.style.height = barHeight + "px";
     }
 
-    requestAnimationFrame(render);
+    // At the next animation frame, call ourselves
+    window.requestAnimationFrame(renderFrame);
   }
-  requestAnimationFrame(render);
 
-  // Make a audio node
-  var audio = new Audio();
-  audio.loop = true;
-  audio.autoplay = true;
-  audio.crossOrigin = "anonymous";
-  audio.src = "https://greggman.github.io/doodles/sounds/DOCTOR VOX - Level Up.mp3";
+  renderFrame();
 
-  audio.addEventListener("canplay", function () {
-      var source = context.createMediaElementSource(audio);
-      source.connect(analyser);
-      analyser.connect(context.destination);
-  });
+  audio.volume = 0.1;
   audio.play();
 }
-
-var clickElem = document.querySelector("button");
-clickElem.addEventListener("click", function () {
-  clickElem.style.display = "none";
-  start();
-});
