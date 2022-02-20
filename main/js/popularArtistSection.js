93785d3b06f440902ede180e7123478024fdb20e
Faria-karim-porna <faria-karim-porna@users.noreply.github.com>
2022-02-20 09:17:12 +0000

    Fix Slider In Popular Artist Section

--- main/js/popularArtistSection.js
+++ main/js/popularArtistSection.js
@@ -1,34 +1,40 @@
-const initialXRight = document
-  .getElementsByClassName("popular-artist-slider")[0]
-  .getBoundingClientRect().right;
-
-const initialXLeft = document
-  .getElementsByClassName("popular-artist-slider")[0]
-  .getBoundingClientRect().left;
-
+document.getElementsByClassName("popular-artist-slider")[0].style.transition = "0.5s";
 function forwardButton() {
-  const laterX = document
-    .getElementsByClassName("popular-artist-slider")[0]
-    .getBoundingClientRect().right;
-
-  const translateValue = ((laterX - initialXRight) / 210 + 1) * 210;
-
-  document.getElementsByClassName(
-    "popular-artist-slider"
-  )[0].style.transform = `translateX(${translateValue}px)`;
-  document.getElementsByClassName("popular-artist-slider")[0].style.transition =
-    "0.5s";
+  if ((parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) !== -1470) {
+    const translateValue = (parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) - 210;
+    document.getElementsByClassName("popular-artist-slider")[0].style.right = `${translateValue}px`;
+  }
 }
 
 function backwardButton() {
-  const laterX = document
-    .getElementsByClassName("popular-artist-slider")[0]
-    .getBoundingClientRect().left;
-
-  const translateValue = ((initialXLeft - laterX) / 210 + 1) * 210;
-  document.getElementsByClassName(
-    "popular-artist-slider"
-  )[0].style.transform = `translateX(-${translateValue}px)`;
-  document.getElementsByClassName("popular-artist-slider")[0].style.transition =
-    "0.5s";
+  if ((parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) !== 0) {
+    const translateValue = (parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) + 210;
+    document.getElementsByClassName("popular-artist-slider")[0].style.right = `${translateValue}px`;
+  }
 }
+
+// function updateForward() {
+//   const forwardInterval = setInterval(() => {
+//     if ((parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) !== -1470) {
+//       const translateValue = (parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) - 210;
+//       document.getElementsByClassName("popular-artist-slider")[0].style.right = `${translateValue}px`;
+//     } else {
+//       clearInterval(forwardInterval);
+//       updateBackward();
+//     }
+//   }, 1000);
+// }
+
+// function updateBackward() {
+//   const backwardInterval = setInterval(() => {
+//     if ((parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) !== 0) {
+//       const translateValue = (parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) + 210;
+//       document.getElementsByClassName("popular-artist-slider")[0].style.right = `${translateValue}px`;
+//     } else {
+//       clearInterval(backwardInterval);
+//       updateForward();
+//     }
+//   }, 1000);
+// }
+
+// updateForward();
