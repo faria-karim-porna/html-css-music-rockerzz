17f20c3d96d04556b14c5f556ef4298b59db84bd
faria-karim-porna <faria.porna.08@gmail.com>
2022-02-20 19:22:46 +0000

    Change Popular Artists Function Names

--- main/js/popularArtistSection.js
+++ main/js/popularArtistSection.js
@@ -1,12 +1,12 @@
 document.getElementsByClassName("popular-artist-slider")[0].style.transition = "0.5s";
-function forwardButton() {
+function popularArtistForwardButton() {
   if ((parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) !== -1470) {
     const translateValue = (parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) - 210;
     document.getElementsByClassName("popular-artist-slider")[0].style.right = `${translateValue}px`;
   }
 }
 
-function backwardButton() {
+function popularArtistBackwardButton() {
   if ((parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) !== 0) {
     const translateValue = (parseInt(document.getElementsByClassName("popular-artist-slider")[0].style.right, 10) || 0) + 210;
     document.getElementsByClassName("popular-artist-slider")[0].style.right = `${translateValue}px`;
