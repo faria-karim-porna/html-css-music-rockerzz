7ba138113bc000e156cebbe8af15b20d83e0abe7
Faria-karim-porna <faria-karim-porna@users.noreply.github.com>
2022-03-16 12:30:39 +0000

    Fix Add Admin Modal HTML CSS JS

--- main/js/addAdminModal.js
+++ main/js/addAdminModal.js
@@ -1,11 +1,8 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var modal = document.getElementsByClassName("addAdminModal")[0];
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("addAdminModal-close")[0];
 
 // When the user clicks the button, open the modal
 function addAdminModalOpen() {
