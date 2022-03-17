02491ab1113416e35d328bf8892c0cdca836c8cf
Faria-karim-porna <faria-karim-porna@users.noreply.github.com>
2022-03-17 08:57:35 +0000

    Add Video Music Modal Complete

--- main/js/dragAndDropModal.js
+++ main/js/dragAndDropModal.js
@@ -1,27 +1,50 @@
-// Get the modal
-var modal = document.getElementsByClassName("addAudioModal")[0];
+// Get the audio modal
+var audioModal = document.getElementsByClassName("addAudioModal")[0];
 
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the audio modal
 var span = document.getElementsByClassName("addAudioModal-close")[0];
 
-// When the user clicks the button, open the modal
+// When the user clicks the button, open the audio modal
 function addAudioModalOpen() {
-  modal.style.display = "block";
+  audioModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the audio modal
 span.onclick = function () {
-  modal.style.display = "none";
+  audioModal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the audio modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == audioModal) {
+    audioModal.style.display = "none";
   }
 };
 
-///////////////////////////////////drag and drop handle////////////////////////////////
+// Get the video modal
+var videoModal = document.getElementsByClassName("addVideoModal")[0];
+
+// Get the <span> element that closes the video modal
+var span = document.getElementsByClassName("addVideoModal-close")[0];
+
+// When the user clicks the button, open the video modal
+function addVideoModalOpen() {
+  videoModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the video modal
+span.onclick = function () {
+  videoModal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the video modal, close it
+window.onclick = function (event) {
+  if (event.target == videoModal) {
+    videoModal.style.display = "none";
+  }
+};
+
+///////////////////////////////////audio drag and drop handle////////////////////////////////
 // Enhanced for my own purposes
 
 var audioDropZoneOverlay = document.getElementsByClassName("drop-zone-overlay")[0];
@@ -80,3 +103,62 @@ audioDropZoneOverlay.addEventListener("dragleave", function (e) {
 
 // 4
 audioDropZoneOverlay.addEventListener("drop", handleAudioDrop);
+
+///////////////////////////////////video drag and drop handle////////////////////////////////
+
+var videoDropZoneOverlay = document.getElementsByClassName("drop-zone-overlay")[1];
+var videoDropZone = document.getElementsByClassName("drop-zone")[1];
+
+function showVideoDropZoneOverlay() {
+  videoDropZoneOverlay.style.display = "block";
+}
+function hideVideoDropZoneOverlay() {
+  videoDropZoneOverlay.style.display = "none";
+}
+
+function allowVideoDrag(e) {
+  if (true) {
+    // Test that the item being dragged is a valid one
+    e.dataTransfer.dropEffect = "copy";
+    e.preventDefault();
+  }
+}
+
+function handleVideoDrop(e) {
+  e.preventDefault();
+  hideVideoDropZoneOverlay();
+
+  if (e.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < e.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (e.dataTransfer.items[i].kind === "file") {
+        var file = e.dataTransfer.items[i].getAsFile();
+        console.log("... file[" + i + "].name = " + file.name);
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < e.dataTransfer.files.length; i++) {
+      console.log("... file[" + i + "].name = " + e.dataTransfer.files[i].name);
+    }
+  }
+}
+
+// 1
+videoDropZone.addEventListener("dragenter", function (e) {
+  showVideoDropZoneOverlay();
+});
+
+2;
+videoDropZoneOverlay.addEventListener("dragenter", allowVideoDrag);
+videoDropZoneOverlay.addEventListener("dragover", allowVideoDrag);
+
+// 3
+videoDropZoneOverlay.addEventListener("dragleave", function (e) {
+  console.log("dragleave");
+  hideVideoDropZoneOverlay();
+});
+
+// 4
+videoDropZoneOverlay.addEventListener("drop", handleVideoDrop);
