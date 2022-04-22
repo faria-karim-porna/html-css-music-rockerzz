b13ff2f0808dc3f4c6f461b0225ec78cc9c1f98c
Faria-karim-porna <faria-karim-porna@users.noreply.github.com>
2022-04-22 04:27:11 +0000

    Fix Responsiveness Of Circular Slider Without Mobile

--- main/js/circularSection.js
+++ main/js/circularSection.js
@@ -1,116 +1,116 @@
-document.getElementsByClassName("disk-1")[0].style.left = "25%";
-document.getElementsByClassName("disk-1")[0].style.transform = "translate(-25%, 0)";
-document.getElementsByClassName("disk-1")[0].style.top = "calc(8vw + 10px)";
+document.getElementsByClassName("disk-1")[0].style.left = "-25%";
+document.getElementsByClassName("disk-1")[0].style.transform = "translate(0%, 0)";
+document.getElementsByClassName("disk-1")[0].style.top = "calc(24vw + 10px)";
 document.getElementsByClassName("disk-1")[0].style.transition = "0.5s";
 
-document.getElementsByClassName("disk-2")[0].style.left = "50%";
-document.getElementsByClassName("disk-2")[0].style.transform = "translate(-50%, 0)";
-document.getElementsByClassName("disk-2")[0].style.top = "10px";
+document.getElementsByClassName("disk-2")[0].style.left = "0%";
+document.getElementsByClassName("disk-2")[0].style.transform = "translate(0%, 0)";
+document.getElementsByClassName("disk-2")[0].style.top = "calc(8vw + 10px)";
 document.getElementsByClassName("disk-2")[0].style.transition = "0.5s";
 
-document.getElementsByClassName("disk-3")[0].style.left = "75%";
-document.getElementsByClassName("disk-3")[0].style.transform = "translate(-75%, 0)";
-document.getElementsByClassName("disk-3")[0].style.top = "calc(8vw + 10px)";
+document.getElementsByClassName("disk-3")[0].style.left = "50%";
+document.getElementsByClassName("disk-3")[0].style.transform = "translate(-50%, 0)";
+document.getElementsByClassName("disk-3")[0].style.top = "calc(0vw + 10px)";
 document.getElementsByClassName("disk-3")[0].style.transition = "0.5s";
 
 document.getElementsByClassName("disk-4")[0].style.left = "100%";
 document.getElementsByClassName("disk-4")[0].style.transform = "translate(-100%, 0)";
-document.getElementsByClassName("disk-4")[0].style.top = "510px";
+document.getElementsByClassName("disk-4")[0].style.top = "calc(8vw + 10px)";
 document.getElementsByClassName("disk-4")[0].style.transition = "0.5s";
 
-document.getElementsByClassName("disk-5")[0].style.left = "75%";
-document.getElementsByClassName("disk-5")[0].style.transform = "translate(-75%, 0)";
-document.getElementsByClassName("disk-5")[0].style.top = "860px";
+document.getElementsByClassName("disk-5")[0].style.left = "125%";
+document.getElementsByClassName("disk-5")[0].style.transform = "translate(-100%, 0)";
+document.getElementsByClassName("disk-5")[0].style.top = "calc(24vw + 10px)";
 document.getElementsByClassName("disk-5")[0].style.transition = "0.5s";
 
-document.getElementsByClassName("disk-6")[0].style.left = "50%";
-document.getElementsByClassName("disk-6")[0].style.transform = "translate(-50%, 0)";
-document.getElementsByClassName("disk-6")[0].style.top = "1010px";
+document.getElementsByClassName("disk-6")[0].style.left = "100%";
+document.getElementsByClassName("disk-6")[0].style.transform = "translate(-100%, 0)";
+document.getElementsByClassName("disk-6")[0].style.top = "calc(40vw + 10px)";
 document.getElementsByClassName("disk-6")[0].style.transition = "0.5s";
 
-document.getElementsByClassName("disk-7")[0].style.left = "25%";
-document.getElementsByClassName("disk-7")[0].style.transform = "translate(-25%, 0)";
-document.getElementsByClassName("disk-7")[0].style.top = "860px";
+document.getElementsByClassName("disk-7")[0].style.left = "50%";
+document.getElementsByClassName("disk-7")[0].style.transform = "translate(-50%, 0)";
+document.getElementsByClassName("disk-7")[0].style.top = "calc(48vw + 10px)";
 document.getElementsByClassName("disk-7")[0].style.transition = "0.5s";
 
 document.getElementsByClassName("disk-8")[0].style.left = "0%";
-document.getElementsByClassName("disk-8")[0].style.transform = "translate(0%, 0)";
-document.getElementsByClassName("disk-8")[0].style.top = "510px";
+document.getElementsByClassName("disk-8")[0].style.transform = "translate(-0%, 0)";
+document.getElementsByClassName("disk-8")[0].style.top = "calc(40vw + 10px)";
 document.getElementsByClassName("disk-8")[0].style.transition = "0.5s";
 
 function circularLeftTopValueForward(className) {
   const leftValue = document.getElementsByClassName(className)[0].style.left;
   const topValue = document.getElementsByClassName(className)[0].style.top;
-  if (leftValue === "25%" && topValue === "calc(8vw + 10px)") {
-    document.getElementsByClassName(className)[0].style.left = "50%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-50%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "10px";
-  } else if (leftValue === "50%" && topValue === "10px") {
-    document.getElementsByClassName(className)[0].style.left = "75%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-75%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "calc(8vw + 10px)";
-  } else if (leftValue === "75%" && topValue === "calc(8vw + 10px)") {
-    document.getElementsByClassName(className)[0].style.left = "100%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-100%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "510px";
-  } else if (leftValue === "100%" && topValue === "510px") {
-    document.getElementsByClassName(className)[0].style.left = "75%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-75%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "860px";
-  } else if (leftValue === "75%" && topValue === "860px") {
-    document.getElementsByClassName(className)[0].style.left = "50%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-50%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "1010px";
-  } else if (leftValue === "50%" && topValue === "1010px") {
-    document.getElementsByClassName(className)[0].style.left = "25%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-25%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "860px";
-  } else if (leftValue === "25%" && topValue === "860px") {
+  if (leftValue === "-25%" && topValue === "calc(24vw + 10px)") {
     document.getElementsByClassName(className)[0].style.left = "0%";
     document.getElementsByClassName(className)[0].style.transform = "translate(0%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "510px";
-  } else {
-    document.getElementsByClassName(className)[0].style.left = "25%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-25%, 0)";
     document.getElementsByClassName(className)[0].style.top = "calc(8vw + 10px)";
+  } else if (leftValue === "0%" && topValue === "calc(8vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "50%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(-50%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(0vw + 10px)";
+  } else if (leftValue === "50%" && topValue === "calc(0vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "100%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(-100%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(8vw + 10px)";
+  } else if (leftValue === "100%" && topValue === "calc(8vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "125%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(-100%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(24vw + 10px)";
+  } else if (leftValue === "125%" && topValue === "calc(24vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "100%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(-100%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(40vw + 10px)";
+  } else if (leftValue === "100%" && topValue === "calc(40vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "50%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(-50%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(48vw + 10px)";
+  } else if (leftValue === "50%" && topValue === "calc(48vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "0%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(0%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(40vw + 10px)";
+  } else {
+    document.getElementsByClassName(className)[0].style.left = "-25%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(0%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(24vw + 10px)";
   }
 }
 
 function circularLeftTopValueBackward(className) {
   const leftValue = document.getElementsByClassName(className)[0].style.left;
   const topValue = document.getElementsByClassName(className)[0].style.top;
-  if (leftValue === "25%" && topValue === "calc(8vw + 10px)") {
+  if (leftValue === "-25%" && topValue === "calc(24vw + 10px)") {
     document.getElementsByClassName(className)[0].style.left = "0%";
     document.getElementsByClassName(className)[0].style.transform = "translate(0%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "510px";
-  } else if (leftValue === "50%" && topValue === "10px") {
-    document.getElementsByClassName(className)[0].style.left = "25%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-25%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(40vw + 10px)";
+  } else if (leftValue === "0%" && topValue === "calc(8vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "-25%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(0%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(24vw + 10px)";
+  } else if (leftValue === "50%" && topValue === "calc(0vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "0%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(0%, 0)";
     document.getElementsByClassName(className)[0].style.top = "calc(8vw + 10px)";
-  } else if (leftValue === "75%" && topValue === "calc(8vw + 10px)") {
+  } else if (leftValue === "100%" && topValue === "calc(8vw + 10px)") {
     document.getElementsByClassName(className)[0].style.left = "50%";
     document.getElementsByClassName(className)[0].style.transform = "translate(-50%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "10px";
-  } else if (leftValue === "100%" && topValue === "510px") {
-    document.getElementsByClassName(className)[0].style.left = "75%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-75%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "calc(8vw + 10px)";
-  } else if (leftValue === "75%" && topValue === "860px") {
+    document.getElementsByClassName(className)[0].style.top = "calc(0vw + 10px)";
+  } else if (leftValue === "125%" && topValue === "calc(24vw + 10px)") {
     document.getElementsByClassName(className)[0].style.left = "100%";
     document.getElementsByClassName(className)[0].style.transform = "translate(-100%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "510px";
-  } else if (leftValue === "50%" && topValue === "1010px") {
-    document.getElementsByClassName(className)[0].style.left = "75%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-75%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "860px";
-  } else if (leftValue === "25%" && topValue === "860px") {
+    document.getElementsByClassName(className)[0].style.top = "calc(8vw + 10px)";
+  } else if (leftValue === "100%" && topValue === "calc(40vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "125%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(-100%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(24vw + 10px)";
+  } else if (leftValue === "50%" && topValue === "calc(48vw + 10px)") {
+    document.getElementsByClassName(className)[0].style.left = "100%";
+    document.getElementsByClassName(className)[0].style.transform = "translate(-100%, 0)";
+    document.getElementsByClassName(className)[0].style.top = "calc(40vw + 10px)";
+  } else {
     document.getElementsByClassName(className)[0].style.left = "50%";
     document.getElementsByClassName(className)[0].style.transform = "translate(-50%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "1010px";
-  } else {
-    document.getElementsByClassName(className)[0].style.left = "25%";
-    document.getElementsByClassName(className)[0].style.transform = "translate(-25%, 0)";
-    document.getElementsByClassName(className)[0].style.top = "860px";
+    document.getElementsByClassName(className)[0].style.top = "calc(48vw + 10px)";
   }
 }
 
