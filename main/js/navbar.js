0857938fc9bb749a93242a3db635138b51f42ed8
Faria-karim-porna <faria-karim-porna@users.noreply.github.com>
2022-04-24 13:33:02 +0000

    Added User Dropdown Function

--- main/js/navbar.js
+++ main/js/navbar.js
@@ -1,5 +1,17 @@
 // toggle overlay navbar
 function openNav() {
-    document.getElementById("myNav").classList.toggle("menu-width");
-    document.querySelector(".custom-menu-btn").classList.toggle("menu-btn-style");
+  document.getElementById("myNav").classList.toggle("menu-width");
+  document.querySelector(".custom-menu-btn").classList.toggle("menu-btn-style");
 }
+document.getElementsByClassName("user-dropdown")[0].style.display = "none";
+
+function openLoginDropdown() {
+  document.getElementsByClassName("user-dropdown")[0].style.display = "flex";
+}
+
+window.onclick = function (event) {
+    console.log(document.getElementById("user-option").onclick)
+  if (!document.getElementById("user-option").onclick) {
+    document.getElementsByClassName("user-dropdown")[0].style.display = "none";
+  }
+};
