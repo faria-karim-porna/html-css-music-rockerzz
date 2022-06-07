ee0692c04d4d8ffaaf84b443a5c3b94ad4e69cd6
faria-karim-porna <faria.porna.08@gmail.com>
2022-06-07 20:17:58 +0000

    Fixed Playlist Section And Popular Album Section Of Audio.html

--- main/js/popularVideoAlbumSection.js
+++ main/js/popularVideoAlbumSection.js
@@ -1,37 +1,17 @@
 const device = window.matchMedia("(max-width: 575.98px)");
 
-const sliderHeight = document.getElementById(
-  "album-card-border-id"
-).offsetHeight;
-const sliderMobileHeight = document.getElementById(
-  "album-card-border-mobile-id"
-).offsetHeight;
+const sliderHeight = document.getElementById("album-card-border-id").offsetHeight;
+const sliderMobileHeight = document.getElementById("album-card-border-mobile-id").offsetHeight;
 
 if (device.matches) {
-  document.getElementsByClassName(
-    "popular-album-slider-section"
-  )[0].style.height = sliderMobileHeight * 1.15 + "px";
-  document.getElementsByClassName("popular-album-slider")[1].style.transition =
-    "1s";
+  document.getElementsByClassName("popular-album-slider-section")[0].style.height = sliderMobileHeight * 1.15 + "px";
+  document.getElementsByClassName("popular-album-slider")[1].style.transition = "1s";
 
   function updatePopularAlbumSectionForward() {
     const forwardInterval = setInterval(() => {
-      if (
-        (parseInt(
-          document.getElementsByClassName("popular-album-slider")[1].style
-            .right,
-          10
-        ) || 0) !== -600
-      ) {
-        const translateValue =
-          (parseInt(
-            document.getElementsByClassName("popular-album-slider")[1].style
-              .right,
-            10
-          ) || 0) - 75;
-        document.getElementsByClassName(
-          "popular-album-slider"
-        )[1].style.right = `${translateValue}vw`;
+      if ((parseInt(document.getElementsByClassName("popular-album-slider")[1].style.right, 10) || 0) !== -600) {
+        const translateValue = (parseInt(document.getElementsByClassName("popular-album-slider")[1].style.right, 10) || 0) - 75;
+        document.getElementsByClassName("popular-album-slider")[1].style.right = `${translateValue}vw`;
       } else {
         clearInterval(forwardInterval);
         updatePopularAlbumSectionBackward();
@@ -41,22 +21,9 @@ if (device.matches) {
 
   function updatePopularAlbumSectionBackward() {
     const backwardInterval = setInterval(() => {
-      if (
-        (parseInt(
-          document.getElementsByClassName("popular-album-slider")[1].style
-            .right,
-          10
-        ) || 0) !== 0
-      ) {
-        const translateValue =
-          (parseInt(
-            document.getElementsByClassName("popular-album-slider")[1].style
-              .right,
-            10
-          ) || 0) + 75;
-        document.getElementsByClassName(
-          "popular-album-slider"
-        )[1].style.right = `${translateValue}vw`;
+      if ((parseInt(document.getElementsByClassName("popular-album-slider")[1].style.right, 10) || 0) !== 0) {
+        const translateValue = (parseInt(document.getElementsByClassName("popular-album-slider")[1].style.right, 10) || 0) + 75;
+        document.getElementsByClassName("popular-album-slider")[1].style.right = `${translateValue}vw`;
       } else {
         clearInterval(backwardInterval);
         updatePopularAlbumSectionForward();
@@ -66,30 +33,14 @@ if (device.matches) {
 
   updatePopularAlbumSectionForward();
 } else {
-  document.getElementsByClassName(
-    "popular-album-slider-section"
-  )[0].style.height = sliderHeight * 1.15 + "px";
-  document.getElementsByClassName("popular-album-slider")[0].style.transition =
-    "1s";
+  document.getElementsByClassName("popular-album-slider-section")[0].style.height = sliderHeight * 1.15 + "px";
+  document.getElementsByClassName("popular-album-slider")[0].style.transition = "1s";
 
   function updatePopularAlbumSectionForward() {
     const forwardInterval = setInterval(() => {
-      if (
-        (parseInt(
-          document.getElementsByClassName("popular-album-slider")[0].style
-            .right,
-          10
-        ) || 0) !== -150
-      ) {
-        const translateValue =
-          (parseInt(
-            document.getElementsByClassName("popular-album-slider")[0].style
-              .right,
-            10
-          ) || 0) - 75;
-        document.getElementsByClassName(
-          "popular-album-slider"
-        )[0].style.right = `${translateValue}vw`;
+      if ((parseInt(document.getElementsByClassName("popular-album-slider")[0].style.right, 10) || 0) !== -150) {
+        const translateValue = (parseInt(document.getElementsByClassName("popular-album-slider")[0].style.right, 10) || 0) - 75;
+        document.getElementsByClassName("popular-album-slider")[0].style.right = `${translateValue}vw`;
       } else {
         clearInterval(forwardInterval);
         updatePopularAlbumSectionBackward();
@@ -99,22 +50,9 @@ if (device.matches) {
 
   function updatePopularAlbumSectionBackward() {
     const backwardInterval = setInterval(() => {
-      if (
-        (parseInt(
-          document.getElementsByClassName("popular-album-slider")[0].style
-            .right,
-          10
-        ) || 0) !== 0
-      ) {
-        const translateValue =
-          (parseInt(
-            document.getElementsByClassName("popular-album-slider")[0].style
-              .right,
-            10
-          ) || 0) + 75;
-        document.getElementsByClassName(
-          "popular-album-slider"
-        )[0].style.right = `${translateValue}vw`;
+      if ((parseInt(document.getElementsByClassName("popular-album-slider")[0].style.right, 10) || 0) !== 0) {
+        const translateValue = (parseInt(document.getElementsByClassName("popular-album-slider")[0].style.right, 10) || 0) + 75;
+        document.getElementsByClassName("popular-album-slider")[0].style.right = `${translateValue}vw`;
       } else {
         clearInterval(backwardInterval);
         updatePopularAlbumSectionForward();
