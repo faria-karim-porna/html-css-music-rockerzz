7b25b58be23645b0d22402a377cf98f34c0f6d58
faria-karim-porna <faria.porna.08@gmail.com>
2022-06-07 12:44:07 +0000

    Fixed Navbar Of Video.html, Fixed Popular Album Of Audio.html And Video.html

--- main/js/popularVideoAlbumSection.js
+++ main/js/popularVideoAlbumSection.js
@@ -1,27 +1,126 @@
-document.getElementsByClassName("video-album-slider")[0].style.transition = "1s";
+const device = window.matchMedia("(max-width: 575.98px)");
 
-function updateVideoAlbumSectionForward() {
-  const forwardInterval = setInterval(() => {
-    if ((parseInt(document.getElementsByClassName("video-album-slider")[0].style.right, 10) || 0) !== -2100) {
-      const translateValue = (parseInt(document.getElementsByClassName("video-album-slider")[0].style.right, 10) || 0) - 1050;
-      document.getElementsByClassName("video-album-slider")[0].style.right = `${translateValue}px`;
-    } else {
-      clearInterval(forwardInterval);
-      updateVideoAlbumSectionBackward();
-    }
-  }, 2000);
+const sliderHeight = document.getElementById(
+  "album-card-border-id"
+).offsetHeight;
+const sliderMobileHeight = document.getElementById(
+  "album-card-border-mobile-id"
+).offsetHeight;
+
+if (device.matches) {
+  document.getElementsByClassName(
+    "popular-album-slider-section"
+  )[0].style.height = sliderMobileHeight * 1.15 + "px";
+  document.getElementsByClassName("popular-album-slider")[1].style.transition =
+    "1s";
+
+  function updatePopularAlbumSectionForward() {
+    const forwardInterval = setInterval(() => {
+      if (
+        (parseInt(
+          document.getElementsByClassName("popular-album-slider")[1].style
+            .right,
+          10
+        ) || 0) !== -600
+      ) {
+        const translateValue =
+          (parseInt(
+            document.getElementsByClassName("popular-album-slider")[1].style
+              .right,
+            10
+          ) || 0) - 75;
+        document.getElementsByClassName(
+          "popular-album-slider"
+        )[1].style.right = `${translateValue}vw`;
+      } else {
+        clearInterval(forwardInterval);
+        updatePopularAlbumSectionBackward();
+      }
+    }, 2000);
+  }
+
+  function updatePopularAlbumSectionBackward() {
+    const backwardInterval = setInterval(() => {
+      if (
+        (parseInt(
+          document.getElementsByClassName("popular-album-slider")[1].style
+            .right,
+          10
+        ) || 0) !== 0
+      ) {
+        const translateValue =
+          (parseInt(
+            document.getElementsByClassName("popular-album-slider")[1].style
+              .right,
+            10
+          ) || 0) + 75;
+        document.getElementsByClassName(
+          "popular-album-slider"
+        )[1].style.right = `${translateValue}vw`;
+      } else {
+        clearInterval(backwardInterval);
+        updatePopularAlbumSectionForward();
+      }
+    }, 2000);
+  }
+
+  updatePopularAlbumSectionForward();
+} else {
+  document.getElementsByClassName(
+    "popular-album-slider-section"
+  )[0].style.height = sliderHeight * 1.15 + "px";
+  document.getElementsByClassName("popular-album-slider")[0].style.transition =
+    "1s";
+
+  function updatePopularAlbumSectionForward() {
+    const forwardInterval = setInterval(() => {
+      if (
+        (parseInt(
+          document.getElementsByClassName("popular-album-slider")[0].style
+            .right,
+          10
+        ) || 0) !== -150
+      ) {
+        const translateValue =
+          (parseInt(
+            document.getElementsByClassName("popular-album-slider")[0].style
+              .right,
+            10
+          ) || 0) - 75;
+        document.getElementsByClassName(
+          "popular-album-slider"
+        )[0].style.right = `${translateValue}vw`;
+      } else {
+        clearInterval(forwardInterval);
+        updatePopularAlbumSectionBackward();
+      }
+    }, 2000);
+  }
+
+  function updatePopularAlbumSectionBackward() {
+    const backwardInterval = setInterval(() => {
+      if (
+        (parseInt(
+          document.getElementsByClassName("popular-album-slider")[0].style
+            .right,
+          10
+        ) || 0) !== 0
+      ) {
+        const translateValue =
+          (parseInt(
+            document.getElementsByClassName("popular-album-slider")[0].style
+              .right,
+            10
+          ) || 0) + 75;
+        document.getElementsByClassName(
+          "popular-album-slider"
+        )[0].style.right = `${translateValue}vw`;
+      } else {
+        clearInterval(backwardInterval);
+        updatePopularAlbumSectionForward();
+      }
+    }, 2000);
+  }
+
+  updatePopularAlbumSectionForward();
 }
-
-function updateVideoAlbumSectionBackward() {
-  const backwardInterval = setInterval(() => {
-    if ((parseInt(document.getElementsByClassName("video-album-slider")[0].style.right, 10) || 0) !== 0) {
-      const translateValue = (parseInt(document.getElementsByClassName("video-album-slider")[0].style.right, 10) || 0) + 1050;
-      document.getElementsByClassName("video-album-slider")[0].style.right = `${translateValue}px`;
-    } else {
-      clearInterval(backwardInterval);
-      updateVideoAlbumSectionForward();
-    }
-  }, 2000);
-}
-
-updateVideoAlbumSectionForward();
