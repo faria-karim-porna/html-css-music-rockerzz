efed464c6991b2ef39cec14563c18caef96618aa
faria-karim-porna <faria.porna.08@gmail.com>
2022-06-07 21:38:54 +0000

    Fixed Video Player Section Of Video.html

--- main/js/video.js
+++ main/js/video.js
@@ -1,14 +1,15 @@
 let isPlayingVideo = false;
 // Get the video element tag
 const video = document.querySelector("video");
-video.src = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+video.src =
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 video.poster = "images/1.jpg";
-
+const mobile = window.matchMedia("(max-width: 575.98px)");
 function playVideo() {
   isPlayingVideo = true;
 
   //  Remove Pause Icon and Add Play Icon
-  const icon = document.getElementById("video-play-pause-icon");
+  const icon = document.getElementById("play-pause-icon");
   icon.classList.remove("fa-play-circle");
   icon.classList.add("fa-pause-circle");
   video.volume = 1;
@@ -20,7 +21,7 @@ function pauseVideo() {
   isPlayingVideo = false;
 
   //  Remove Play Icon and Add Pause Icon
-  const icon = document.getElementById("video-play-pause-icon");
+  const icon = document.getElementById("play-pause-icon");
   icon.classList.remove("fa-pause-circle");
   icon.classList.add("fa-play-circle");
 
@@ -33,38 +34,57 @@ function playPauseVideo() {
 
 function changeVideoDuration() {
   //change video time based on slider
-  let durationSlider = document.querySelector("#video-duration-slider");
+  let durationSlider = document.querySelector("#duration-slider");
   const sliderPosition = video.duration * (durationSlider.value / 100);
   video.currentTime = sliderPosition;
   const sliderPoint = video.currentTime * (100 / video.duration);
   // change slider fill
-  durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPoint}%, white ${sliderPoint + 0.1}%)`;
+  durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPoint}%, white ${
+    sliderPoint + 0.1
+  }%)`;
 }
 
 function changeVideoVolume() {
-  //change audio volume based on slider
-  let volumeSlider = document.querySelector("#video-volume-slider");
-  const volumeSliderPosition = volumeSlider.value / 100;
-  video.volume = volumeSliderPosition;
-  const volumeSliderPoint = volumeSliderPosition * 100;
-  // change volume slider fill
-  volumeSlider.style.background = `linear-gradient(90deg, transparent ${volumeSliderPoint}%, white ${volumeSliderPoint + 0.1}%)`;
+  if (mobile.matches) {
+    //mobile version
+    //change audio volume based on slider
+    let volumeSliderMobile = document.querySelector("#volume-slider");
+    const volumeSliderPositionMobile = volumeSliderMobile.value / 100;
+    video.volume = volumeSliderPositionMobile;
+    const volumeSliderPointMobile = volumeSliderPositionMobile * 100;
+    // change volume slider fill
+    volumeSliderMobile.style.background = `linear-gradient(90deg, transparent ${volumeSliderPointMobile}%, white ${
+      volumeSliderPointMobile + 0.1
+    }%)`;
+  } else {
+    //change audio volume based on slider
+    let volumeSlider = document.querySelector("#volume-slider-video");
+    const volumeSliderPosition = volumeSlider.value / 100;
+    video.volume = volumeSliderPosition;
+    const volumeSliderPoint = volumeSliderPosition * 100;
+    // change volume slider fill
+    volumeSlider.style.background = `linear-gradient(90deg, transparent ${volumeSliderPoint}%, white ${
+      volumeSliderPoint + 0.1
+    }%)`;
+  }
 }
 
 function upDateDurationVideo() {
   //change slider based on video time
-  let durationSlider = document.querySelector("#video-duration-slider");
+  let durationSlider = document.querySelector("#duration-slider");
   if (!isNaN(video.duration)) {
     const sliderPosition = video.currentTime * (100 / video.duration);
     durationSlider.value = sliderPosition;
     // change slider fill
-    durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPosition}%, white ${sliderPosition + 0.1}%)`;
+    durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPosition}%, white ${
+      sliderPosition + 0.1
+    }%)`;
   }
 }
 
 function forwardDurationVideo() {
   //change slider based on audio time
-  let durationSlider = document.querySelector("#video-duration-slider");
+  let durationSlider = document.querySelector("#duration-slider");
   if (!isNaN(video.duration)) {
     // to speed up the video
     // video.playbackRate = video.playbackRate + 0.5;
@@ -73,13 +93,15 @@ function forwardDurationVideo() {
     const sliderPosition = video.currentTime * (100 / video.duration);
     durationSlider.value = sliderPosition;
     // change slider fill
-    durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPosition}%, white ${sliderPosition + 0.1}%)`;
+    durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPosition}%, white ${
+      sliderPosition + 0.1
+    }%)`;
   }
 }
 
 function backwardDurationVideo() {
   //change slider based on audio time
-  let durationSlider = document.querySelector("#video-duration-slider");
+  let durationSlider = document.querySelector("#duration-slider");
   if (!isNaN(video.duration)) {
     // to speed down the video
     // video.playbackRate = video.playbackRate - 0.5;
@@ -88,18 +110,37 @@ function backwardDurationVideo() {
     const sliderPosition = video.currentTime * (100 / video.duration);
     durationSlider.value = sliderPosition;
     // change slider fill
-    durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPosition}%, white ${sliderPosition + 0.1}%)`;
+    durationSlider.style.background = `linear-gradient(90deg, transparent ${sliderPosition}%, white ${
+      sliderPosition + 0.1
+    }%)`;
   }
 }
 
 function addToFavoriteVideo() {
-  document.getElementById("favorite-video").classList.toggle("add-to-favorite-video");
+  document.getElementById("favorite-video").classList.toggle("add-to-favorite");
+  document.getElementById("favorite").classList.toggle("add-to-favorite");
 }
 
 setInterval(upDateDurationVideo, 1000);
 
-// change volume slider fill
-const volumeSlider = document.querySelector("#video-volume-slider");
-const volumeSliderPosition = volumeSlider.value / 100;
-const volumeSliderPoint = volumeSliderPosition * 100;
-volumeSlider.style.background = `linear-gradient(90deg, transparent ${volumeSliderPoint}%, white ${volumeSliderPoint + 0.1}%)`;
+function setInitialVolumeFill() {
+  if (mobile.matches) {
+    // change volume slider fill mobile
+    const volumeSliderMobile = document.querySelector("#volume-slider");
+    const volumeSliderPositionMobile = volumeSliderMobile.value / 100;
+    const volumeSliderPointMobile = volumeSliderPositionMobile * 100;
+    volumeSliderMobile.style.background = `linear-gradient(90deg, transparent ${volumeSliderPointMobile}%, white ${
+      volumeSliderPointMobile + 0.1
+    }%)`;
+  } else {
+    // change volume slider fill
+    const volumeSlider = document.querySelector("#volume-slider-video");
+    const volumeSliderPosition = volumeSlider.value / 100;
+    const volumeSliderPoint = volumeSliderPosition * 100;
+    volumeSlider.style.background = `linear-gradient(90deg, transparent ${volumeSliderPoint}%, white ${
+      volumeSliderPoint + 0.1
+    }%)`;
+  }
+}
+
+setInitialVolumeFill();
